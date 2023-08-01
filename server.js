59a10fde7dda0dc107421f0c3f82c24ac7a4b22f
BasitAlizah <141009844+BasitAlizah@users.noreply.github.com>
2023-08-01 13:57:24 +0500

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,25 @@
+// websocket-server.js
 const WebSocket = require('ws');
 const wss = new WebSocket.Server({ port: 8080 });
 
-wss.on('connection', (ws) => {
-    console.log('A new client connected');
+const connectedClients = new Set();
 
-    ws.on('message', (message) => {
-        // Broadcast to all connected clients
-        wss.clients.forEach((client) => {
-            if (client.readyState === WebSocket.OPEN) {
-                client.send(message);
-            }
-        });
+wss.on('connection', (ws) => {
+  console.log('A new client connected');
+
+  connectedClients.add(ws);
+
+  ws.on('message', (message) => {
+    // Broadcast the received message to all connected clients
+    connectedClients.forEach((client) => {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(message);
+      }
     });
+  });
+
+  ws.on('close', () => {
+    console.log('A client disconnected');
+    connectedClients.delete(ws);
+  });
 });
